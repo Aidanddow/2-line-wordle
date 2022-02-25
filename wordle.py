53f16be1b611759a46e894582ab2d865d59555e8
Aidanddow <2468915D@student.gla.ac.uk>
2022-02-25 14:37:08 +0000

    Down to 2 lines

--- wordle.py
+++ wordle.py
@@ -1,5 +1,2 @@
-import random
-get_input, wordList, word, result, tries = (lambda inp="": inp if len(inp)==5 and inp.lower() in wordList else get_input(inp = input("Enter a guess: "))), [i.strip() for i in open("wordlist.txt")], random.choice([i.strip() for i in open("wordlist.txt")]), "", 0
-while result != "🟩🟩🟩🟩🟩" and tries < 6:
-    result, tries = "".join([("🟩" if i==j else "🟨" if i in word else "⬛️") for i,j in zip(get_input().lower(), word.lower())]), tries + 1
-    print(result + ("\nWell Done!" if result=="🟩🟩🟩🟩🟩" else "") + (f"\nUnlucky, word was {word}" if tries > 5 else ""))
+import random; get_input, wordList, word, result, tries = (lambda inp="": inp if len(inp)==5 and inp.lower() in wordList else get_input(inp = input("Enter a guess: "))), [i.strip() for i in open("wordlist.txt")], random.choice([i.strip() for i in open("wordlist.txt")]), "", 0
+while result != "🟩🟩🟩🟩🟩" and tries < 6: result, tries = "".join([("🟩" if i==j else "🟨" if i in word else "⬛️") for i,j in zip(get_input().lower(), word.lower())]), tries + 1; print(result + ("\nWell Done!" if result=="🟩🟩🟩🟩🟩" else "") + (f"\nUnlucky, word was {word}" if tries > 5 else ""))
